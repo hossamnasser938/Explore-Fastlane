980eb2135f7998e8d63da8665740418fdd23b908
Hossam <hossamnasser938@gmail.com>
2022-12-12 17:46:08 +0200

    add multiple android flavors for dev, stag, and prod environments

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import Config from 'react-native-config';
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Explore FastLane</Text>
-  </View>
-);
+const App = () => {
+  console.log('Config', Config)
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Explore FastLane</Text>
+      <Text style={styles.subText}>{Config.ENVIRONMENT}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-  text: {fontSize: 20, fontWeight: '400'}
+  text: {fontSize: 20, fontWeight: '400'},
+  subText: {fontSize: 18},
 });
 
 export default App;
